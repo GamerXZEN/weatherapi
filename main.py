4b84ce75b4c6f8d2ea5cedcccac801a1c3aecf2b
FESL Sapphire Dev <codspecialops@gmail.com>
2023-02-03 10:52:52 -0600

    Added url endpoints and upgraded Weather API to version 2.1

--- main.py
+++ main.py
@@ -3,21 +3,44 @@ import pandas as pd
 
 website = Flask(__name__)
 
+stations = pd.read_csv("stations.txt", skiprows=17)
+
 
 @website.route("/")
 def home():
-	return render_template("index.html")
+	return render_template("index.html", data=stations.to_html())
 
 
-@website.route("/v1.2/<station>/<date>/")
-def web_data(station, date):
-	data = pd.read_csv(f"stations/TG_STAID{str(station).zfill(6)}.txt", skiprows=20, parse_dates=["    DATE"])
+@website.route("/v2.1/<station>/")
+def station_data(station):
+	data = pd.read_csv(f"stations/TG_STAID{str(station).zfill(6)}.txt",
+	                   skiprows=20, parse_dates=["    DATE"])
+	data = data[["STAID", "    DATE", "   TG"]]
+	return render_template("station.html", data=data.to_html())
+
+
+@website.route("/v2.1/<station>/year/<year>")
+def year_data(station, year):
+	data = pd.read_csv(f"stations/TG_STAID{str(station).zfill(6)}.txt",
+	                   skiprows=20)
+	data["    DATE"] = data["    DATE"].astype(str)
+	data = data[["STAID", "    DATE", "   TG"]]
+	result = data[data["    DATE"].str.startswith(str(year))]
+	return render_template("year.html", data=data.to_html())
+
+
+@website.route("/v2.1/<station>/date/<date>/")
+def date_data(station, date):
+	data = pd.read_csv(f"stations/TG_STAID{str(station).zfill(6)}.txt",
+	                   skiprows=20, parse_dates=["    DATE"])
 	temperature = data.loc[data["    DATE"] == date]["   TG"].squeeze() / 10
 	if temperature == -999.9:
 		temperature = "No temperature was recorded"
-	return {"stations": station,
-	        "date": date,
-	        "temperature": temperature}
+	web_station = f"Station: {station}"
+	web_date = f"Date: {date}"
+	web_temperature = f"Temperature in Celsius: {temperature}"
+	return render_template("date.html", html_station=web_station, html_date = web_date, html_temp = web_temperature)
+
 
 if __name__ == "__main__":
 	website.run(debug=True, port=1472)
